309457aa897e3e5666e5bb137eb618d9e0392b7d
NIUWCSP <121147600+NIUWCSP@users.noreply.github.com>
2024-05-22 19:55:06 +0800

    Update watchdog.py

--- watchdog.py
+++ watchdog.py
@@ -1,9 +1,15 @@
 import os
 import json
 import logging
-from datetime import datetime
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import paho.mqtt.client as mqtt
+import threading
+import time
+import subprocess
+
+# 設置重啟docker時間
+how_minutes = 10
 
 # 設置日誌級別
 logging.basicConfig(level=logging.INFO)
@@ -20,6 +26,7 @@ mqtt_password = os.getenv('MQTT_PASSWORD')
 
 # 創建BLOCK_URL空的dictionary
 block_url_dic = dict()
+lock = threading.Lock()
 
 # 定義 MQTT 回調函數
 def on_message(client, userdata, message):
@@ -54,6 +61,35 @@ def on_message(client, userdata, message):
             
     except Exception as e:
         logging.error("Error in on_message: %s", e)
+
+# 定義檢查更新時間的函數
+def check_block_url_times():
+    while True:
+        time.sleep(180)  # 每3分鐘檢查一次
+        with lock:
+            current_time = datetime.utcnow()
+            how_minutes_ago = current_time - timedelta(minutes=how_minutes)
+            # 檢查是否所有 tag 的時間戳都過期
+            all_outdated = all(datetime.fromisoformat(timestamp) < how_minutes_ago for timestamp in block_url_dic.values())
+
+            if all_outdated and block_url_dic:
+                print(f"ERROR: All tags have not been updated in over {how_minutes} minutes")
+                # 嘗試重啟 IOTA_GATEWAY_Docker
+                restart_docker_container('my-iota-app-container')
+
+# 定義重啟 Docker 容器的函數
+def restart_docker_container(container_name):
+    try:
+        subprocess.run(['docker', 'restart', container_name], check=True)
+        logging.info("Successfully restarted Docker container: %s", container_name)
+    except subprocess.CalledProcessError as e:
+        logging.error("Failed to restart Docker container: %s", container_name)
+        logging.error("Error: %s", e)                                
+
+# 初始化並啟動檢查更新時間的線程
+thread = threading.Thread(target=check_block_url_times)
+thread.daemon = True
+thread.start()
 
 # 初始化 MQTT 客戶端
 mqtt_client = mqtt.Client()
